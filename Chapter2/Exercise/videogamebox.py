d8372dc8065620d5c7d1158bcd55927525f43b8d
Julien Pernin <pernin.julien@gmail.com>
2019-06-20 08:07:09 +0200

    add files
    Added files to python object oriented programming's course

---
+++ Chapter2/Exercise/videogamebox.py
@@ -0,0 +1,9 @@
+import datetime
+
+# store the newt available id for all new videogame available
+last_id = 0
+
+
+class VideoGame:
+    """Represent a videogame that will be put inside the videogamebox
+    Match against a string in searches and store tag for each videogame"""
